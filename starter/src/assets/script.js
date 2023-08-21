f15fe0f7ef059492d10db7bc91068364747d0c80
Yikealo Abraha <yikealo@Yikealos-MacBook-Pro.local>
2023-08-21 16:48:31 -0400

    feat: Project requirment fulfiled
    implemented the following required functions\naddProductToCart\nincreaseQuantity\ndecreaseQuantity\nremoveProductFromCart\ncartTotal\npay\n\n code also refactored to confirm es6\n\nExtra features\nemptyCart implemented.

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -46,61 +46,106 @@ products.push(strawberry);
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
+const cart = [];
+
+/* helper function to get product with productID */
+function getProduct(productId) {
+  return products.find((product) => product.productId === productId);
+}
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
-
+function addProductToCart(productId) {
+  const product = getProduct(productId); // find the product with productID
+  product.quantity += 1;
+  if (!cart.includes(product)) {
+    cart.push(product);
+  }
+}
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
-
-/* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
-*/
+function increaseQuantity(productId) {
+  const product = getProduct(productId);
+  product.quantity += 1;
+}
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+function removeProductFromCart(productId) {
+  // remove the proudct with productID form the cart
+  const product = getProduct(productId);
+  product.quantity = 0;
+  const productIndex = cart.findIndex((item) => item.productId === productId);
+  cart.splice(productIndex, 1);
+}
+
+/* Create a function named decreaseQuantity that takes in the productId as an argument
+  - decreaseQuantity should get the correct product based on the productId
+  - decreaseQuantity should decrease the quantity of the product
+  - if the function decreases the quantity to 0, the product is removed from the cart
+*/
+function decreaseQuantity(productId) {
+  const product = getProduct(productId);
+  product.quantity -= 1;
+  if (product.quantity === 0) {
+    removeProductFromCart(productId);
+  }
+}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
+function cartTotal() {
+  let sum = 0;
+  cart.forEach((product) => {
+    sum += (product.quantity * product.price); // get cost of product and add it to the sum
+  });
+
+  return sum;
+}
 
 /* Create a function called emptyCart that empties the products from the cart */
+function emptyCart() {
+  cart.length = 0;
+}
 
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
+function pay(amount) {
+  totalPaid += amount;
+  return totalPaid - cartTotal(); // subtract the cart's total from the ammount paied
+}
 
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
+/* Place stand out suggestions here (stand out suggestions can be found at the bottom
+of the project rubric.) */
 
-
-/* The following is for running unit tests. 
+/* The following is for running unit tests.
    To fully complete this project, it is expected that all tests pass.
    Run the following command in terminal to run tests
    npm run test
 */
 
 module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
-}
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay,
+  emptyCart,
+  /* Uncomment the following line if completing the currency converter bonus */
+  //currency,
+};
